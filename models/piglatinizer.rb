4fd0a5ce3e5bbbb7c58492a5393cd9d1ccc74794
Kelsey Fecho <kfecho30@gmail.com>
2018-04-08 02:17:22 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class PigLatinizer
+  def initialize
+  end
+
+  def piglatinize(text)
+    words = text.split(" ")
+    words.each do |word|
+      binding.pry
+      word.split("").scan(/[aeiou])
+      #from beginning of word, find first vowel and take all consonants before then
+      #add that chunk to the end of the word plus "ay"
+    end
+  end
+
+
+end
